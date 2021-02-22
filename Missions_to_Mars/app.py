640bad912e8878938f006b8df5488888ac5d0a55
walaa <w.alani2010@yahoo.com>
2021-02-21 22:17:37 -0800

    folder initial structure

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,5 @@
+
+
+# news_title = "NASA's Next Mars Mission to Investigate Interior of Red Planet"
+
+# news_p = "Preparation of NASA's next spacecraft to Mars, InSight, has ramped up this summer, on course for launch next May from Vandenberg Air Force Base in central California -- the first interplanetary launch in history from America's West Coast."
