18d2da79a603e611540aff287ad4ee82e35426cf
walaa <w.alani2010@yahoo.com>
2021-03-02 21:10:46 -0800

    modifications

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect
+#pip install flask_pymongo
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config['MONGO URI'] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def init_browser():    
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars = mars)
+
+@app.route("/scrape")
+def scrape():    
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return redirect('/', code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)   
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+from splinter import Browser 
+from bs4 import BeautifulSoup as bs
+import pandas as pd 
+from webdriver_manager.chrome import ChromeDriverManager
 
-app = Flask(__name__)
-
-@app.route("/")
-def init_browser():    
+def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}    
-    return Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=False)
+    news_title, news_p = news(broswer)
+    mars_data_info = {
+        'news_title': news_title,
+        'news_paragraph': news_p,
+        'featured_image': featured_image(browser),
+        'facts': facts(),
+        'hemispheres': img_urls(browser)
+    }
+    browser.quit()
+    return mars_data_info
 
-@app.route("/scrape")
-def scrape():    
-    mars_dict={}    
-    browser = init_browser()
 
-if __name__ == "__main__":
-    app.run(debug=True)   
+def news(browser):
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    # HTML object
+    html = browser.html
+    # Parse HTML with Beautiful Soup
+    soup = bs(html, 'html.parser')
+    # Retrieve all elements that contain latestr news titles 
+    news_title = soup.find_all('div', class_='content_title')[1].text  
+    news_p = soup.find_all('div', class_='rollover_description_inner')[1].text 
+    return news_title, news_p
+
